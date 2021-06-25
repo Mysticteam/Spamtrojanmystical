1b286fa33437fcad310aeaf1211cd4f3404d8138
Mysticteam <86481049+Mysticteam@users.noreply.github.com>
2021-06-25 14:35:47 -0300

    Create Trojanmysticalteam.py

---
+++ Trojanmysticalteam.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# [GCC 4.2.1 Compatible Android (6454773 based on r365631c2) Clang 9.0.8 (https:/                                                   # Embedded file name: dg                                          import sys, os
+from time import sleep
+os.system('clear')
+logo = '𔑮𝐁𝐘࿇𝐀࿇𝐌𝐘𝐒𝐓𝐈𝐂𝐀𝐋࿇𝐓𝐄𝐀𝐌𔒙  •  𓆲⛓𐂘𝐓𝐑𝐎𝐉𝐀𝐍 𝐒𝐏𝐀𝐌𝐌𝐄𝐑𐂘⛓𔒒    𝑷𝑹𝑰𝑽𝑨𝑻𝑬 𝑽𝑬𝑹𝑺𝑰𝑶𝑵  '                                                            print logo                                                        target = raw_input("\x1b[32;1mNúmero do alvo:\x1b[31;1m :\x1b[33;1m ")                                                        sleep(1)                                                          os.system('clear')
+print logo
+print '\x1b[32;1mNúmero enckntrafl ! '                                 print '\x1b[31;1mDeseja enviar para esse número? \x1b[33;1m' + target + '\x1b[31;1m ?'
+lanj = raw_input('\x1b[32;1maperte (y) pra cmc\x1b[33;1m = ')
+if lanj == 'y':                                                       while True:
+        print '\x1b[32;1mTrojan enviado para:\x1b[31;1m :\x1b[33;1m ' + target
